8366786cc07656b128b99f7d096f1624dbc68a6e
DzhangrErdniev21D <85980107+KalmDzhan@users.noreply.github.com>
2023-03-20 21:42:05 +0300

    Add files via upload

---
+++ 10.19/10.19/Program.cs
@@ -0,0 +1,16 @@
+﻿int count = 0;
+
+Console.Write("Введите 3 предложения: ");
+string text = Console.ReadLine().ToLower();
+
+Console.Write("Введите букву, количество которой надо подсчитать: ");
+char letter = Convert.ToChar(Console.ReadLine());
+
+foreach (char symbol in text)
+{
+	if (symbol == letter)
+	{
+		count++;
+	}
+}
+Console.WriteLine("В вашем тексте " + count + " букв " + letter);
